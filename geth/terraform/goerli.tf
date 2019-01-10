10e8094c69bb161bfe4d39ec3393f4d28eea501e
Priom Chowdhury <priom.galaxy@gmail.com>
2019-01-10 05:01:28 -0500

    terraform script and readme updated (#37)

--- geth/terraform/goerli.tf
+++ geth/terraform/goerli.tf
@@ -1,19 +1,19 @@
 provider "google" {
     credentials = "${file("credentials.json")}"
-    project     = "[INSERT_GOOGLE_CLOUD_PROJECT_ID]"
+    project     = "[INSERT_GOOGLE_CLOUD_PROJECT_ID_HERE]"
 }
 
 resource "google_compute_instance" "name" {
-  name = "INSERT_GOOGLE_CLOUD_INSTANC_NAME",
-  zone = "northamerica-northeast1-a",
-  machine_type = "custom-2-4096",
+  name = "INSERT_GOOGLE_CLOUD_INSTANCE_NAME_HERE",
+  zone = "us-east1-b",
+  machine_type = "n1-standard-1",
   min_cpu_platform = "Intel Skylake"
   tags = ["http-server", "https-server"]
 
   boot_disk {
     initialize_params {
         image = "ubuntu-1804-lts"
-        type = "pd-ssd"
+        type = "pd-standard"
         size = "40"
     }
   }
@@ -24,5 +24,5 @@ resource "google_compute_instance" "name" {
     access_config {}
   }
 
-  metadata_startup_script = "sudo apt update -y && sudo apt upgrade -y"
+  metadata_startup_script = "sudo apt update -y && sudo apt upgrade -y && sudo apt-get install software-properties-common -y && sudo add-apt-repository ppa:ethereum/ethereum -y && sudo apt update -y && sudo apt install ethereum -y && sudo curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh && sudo bash nodesource_setup.sh && sudo apt install -y nodejs"
 }
